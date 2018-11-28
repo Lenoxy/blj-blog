a7bbaa5a39ac4be20e4af75ec104995a237aafe1
Lenoxy <lenoxy@gmx.ch>
2018-11-28 15:03:45 +0100

    A002 Able to push messages.

--- index.php
+++ index.php
@@ -17,57 +17,43 @@ $dbh = new PDO('mysql:host=localhost;dbname=blog', $user, $pass);
 
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$isNameValid = true;
+$isTitleValid = true;
+$isPostValid = true;
+
     
 if (isset($_POST["speichern"])) {
     $name = $_POST['name'] ??'';
     $title = $_POST['title']??'';
     $post = $_POST['post']??'';
-    ?> <div class=errordiv> <?php
 
     if(empty($name)){
         ?> <p class=errorbox>Bitte geben Sie Ihr Name an.</p> <?php
+        $isNameValid = false;
     }
 
     if(empty($title)){
         ?> <p class=errorbox>Bitte schreiben Sie ein Titel.</p> <?php
+        $isTitleValid = false;
     }
 
     if(empty($post)){
        ?> <p class=errorbox>Bitte schreiben Sie eine Nachticht.</p> <?php
+       $isPostValid = false;
     }
-    if(empty($name) || empty($title) || empty($post)){
-
+    
+    if($isNameValid===true && $isTitleValid===true && $isPostValid===true){
         
     }
 
 
-    $stmt = $dbh->prepare("INSERT INTO `posts` (created_by, created_at, post_title, post_text) VALUES(:name, :title, :post)");
-    $stmt->execute([`:name` => $name, `:title` => $title, `:post` => $post]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?> </div> <?php
+    if($isNameValid===true && $isTitleValid===true && $isPostValid===true){
+        $stmt = $dbh->prepare("INSERT INTO posts (created_by, post_title, post_text) VALUES(:name, :title, :post)");
+        $stmt->execute([':name' => $name, ':title' => $title, ':post' => $post]);
+    }
 }
-
-
-
-
-
-
-  ?>
+?>
 
 
 
@@ -77,7 +63,7 @@ if (isset($_POST["speichern"])) {
      <form action="index.php" method="post">
         <label for="name"> Name:</label><input id="name" type="text" name="name"><br>
         <label for="title"> Title:</label><input id="title" type="text" name="title"><br>
-        <label for="post"> Text:</label><textarea id="post"  name="post"></textarea><br>
+        <label for="post"> Text:</label><textarea id="post"  name="post" rows="10"></textarea><br>
         <input type="submit" id="submit" name="speichern"><br>
     </form>
  
