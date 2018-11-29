4b7e0b7e95b3331607a9190438250fdd5cd8b286
Lenoxy <lenoxy@gmx.ch>
2018-11-29 13:18:19 +0100

    wiederherstellen.

--- index.php
+++ index.php
@@ -18,9 +18,4 @@
 <h1>Home</h1>
 <img class="front" src="https://media1.tenor.com/images/332fed3af42f8829a0eaa13a21903f72/tenor.gif?itemid=3955347" alt="Tolles gif, ohne welches du nicht leben könntest." width=99%>
  </body>
- asdgfdasf dasf dasf das fdas f
-das fdas f dasf 
  </html> 
-
-
- asdfsdf
